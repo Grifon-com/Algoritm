7854a6bd508f5128d1304f6f7b10e5b96cb6db1f
Grifon <mashukgrisha@mail.ru>
2023-09-21 11:51:52 +0300

    Initial Commit

---
+++ Algoritm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Algoritm
+//
+//  Created by Марина Машук on 21.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
